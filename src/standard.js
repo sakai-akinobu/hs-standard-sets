4944512a4fc8618232c17c95112d2348fddc7346
sakai-akinobu <sakai.akinobu@gmail.com>
2018-08-31 17:23:16 +0900

    Fix card set object structures.

--- src/standard.js
+++ src/standard.js
@@ -1,21 +1,84 @@
 const CARD_SETS = [
-  'TGT',
-  'BOOMSDAY',
-  'BRM',
-  'GANGS',
-  'CORE',
-  'EXPERT1',
-  'HOF',
-  'NAXX',
-  'GILNEAS',
-  'GVG',
-  'HERO_SKINS',
-  'ICECROWN',
-  'KARA',
-  'LOE',
-  'LOOTAPALOOZA',
-  'OG',
-  'UNGORO',
+  {
+    name: 'CORE',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'EXPERT1',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'TGT',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'BRM',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'GANGS',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'HOF',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'NAXX',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'GILNEAS',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'GVG',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'KARA',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'LOE',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'OG',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'UNGORO',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'ICECROWN',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'LOOTAPALOOZA',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
+  {
+    name: 'BOOMSDAY',
+    openedAt: new Date('2010-01-01T00:00:00'),
+    closedAt: new Date('2020-01-01T00:00:00'),
+  },
 ];
 
 export default function() {
